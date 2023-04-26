683b6f4120511c454b1f0d6ac44392063c827288
2645703338 <74071703+2645703338@users.noreply.github.com>
2023-04-26 21:12:18 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ mod utils;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!(
     r#"
-资产阶级国家的形式虽然多种多样，但本质是一样的：所有这些国家，不管怎样，归根到底一定都是资产阶级专政。
-——《国家与革命》，列宁
+青年大学习自动学习程序
+——Powered by Lanny.
         "#
   );
   let args = utils::get_args();
